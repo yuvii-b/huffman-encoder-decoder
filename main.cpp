bcbd495e0fbd7b0ebe13ddd06a6cebad485a9283
yuvaraj <yuvaraj.codes@gmail.com>
2025-06-15 15:24:22 +0000

    added main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "huffman.h"
 
 int main(int argc, char* argv[]){
-	
+	HuffmanCoder *huff = new HuffmanCoder();
+	std::string text;
+	std::cout << "Enter the text to encode: ";
+	std::getline(std::cin, text);
+	std::cout << "Original text is: " << text << std::endl;
+    huff->initialize(text);
+	std::string encoded = huff->encode(text);
+    std::cout << "Encoded: "  << encoded  << std::endl;
+	std::string decoded = huff->decode(encoded);
+	std::cout << "Decoded: "  << decoded << std::endl;
+	delete huff;
 	return 0;
 }
